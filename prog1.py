c619cdd278e7f958a800f4638193d875590df431
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-11-26 13:07:01 +0800

    If the number arraangement will be -b-c-d or a-b-d-c

--- prog1.py
+++ prog1.py
@@ -4,16 +4,35 @@
 # steps
 # 1 ask 4 numbers
 def askNumbers():
-    _first = int(input('First Number:'))
-    _second = int(input('Second Number: '))
-    _third =  int(input('Third Number: ')) 
-    _fourth= int(input('Fourth Number: '))
+    _first = float(input('First Number:'))
+    _second = float(input('Second Number: '))
+    _third =  float(input('Third Number: ')) 
+    _fourth= float(input('Fourth Number: '))
     return _first, _second, _third, _fourth
 
+# determine the highest number of the four
+def arrangeNumbers(_a, _b, _c, _d):
+    # if a is the highest of four
+    if _a > _b and _a > _c and _a > _d:
+        # if the order will be a-b-c-d 
+        if _b > _c and _b > _d :
+            if _c > _d:
+                print(f"{_a}, {_b}, {_c}, {_d}")
 
-# determine the highest numberof the four
-# determine the second highest
-# determine the third highest
-# determine the lowest of the four
-# print the results from the highest number to the lowest
-a,  b, c, d = askNumbers()
+            # if the order will be a-b-c-d
+            else:
+                print (f"{_a}, {_b}, {_d}, {_c}")
+        
+
+        
+
+    
+    
+
+    else:
+        print('others')
+        
+
+a, b, c, d = askNumbers()
+highest = arrangeNumbers(a, b, c, d)
+print(highest)
