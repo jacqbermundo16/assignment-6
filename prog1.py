6891d5d7bc009ee8cfc1a179d0df154507e33955
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-11-26 13:21:35 +0800

    if the second highest is c aand the order will be a-c-b-d or a-c-d-b

--- prog1.py
+++ prog1.py
@@ -14,19 +14,26 @@ def askNumbers():
 def arrangeNumbers(_a, _b, _c, _d):
     # if a is the highest of four
     if _a > _b and _a > _c and _a > _d:
-        # if the order will be a-b-c-d 
+        # if the second highest is b
         if _b > _c and _b > _d :
+            # if the order will be-b-c-d
             if _c > _d:
                 print(f"{_a}, {_b}, {_c}, {_d}")
-
             # if the order will be a-b-c-d
             else:
                 print (f"{_a}, {_b}, {_d}, {_c}")
-        
 
-        
+        # if the second highest is c 
+        elif _c > _b and _c > _d :
+            #if the order will be a-c-b-d
+            if _b > _d:
+                print(f"{_a}, {_c}, {_b}, {_d}") 
+            #if the order will be a-c-d-b
+            else:
+                print(f"{_a}, {_c}, {_d}, {_b}") 
 
-    
+        # if the second highest is d
+        
     
 
     else:
